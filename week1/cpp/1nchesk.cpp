198d135c034834455864313b5a9b6b752727575b
Natalya Cheskina <cheskina.n@gmail.com>
2019-04-19 12:56:28 +0300

    cosmetic changes

--- week1/cpp/1nchesk.cpp
+++ week1/cpp/1nchesk.cpp
@@ -1,41 +1,21 @@
 class Solution {
-
 private:
-
     vector<vector<int>> result;
-
 public:
-
-    void recursive_permute(vector<int> &nums,int start) {
-
+    void recursive_permute(vector<int> &nums, int start) {
         if(start >= nums.size()) 
-
             return;
-
-        recursive_permute(nums, start+1);
-
-        for(int i=0; i<start; i++){
-
+        recursive_permute(nums, start + 1);
+        for(int i = 0; i < start; i++){
             swap(nums[i], nums[start]);
-
             result.push_back(nums);
-
-            recursive_permute(nums, start+1);
-
+            recursive_permute(nums, start + 1);
             swap(nums[i], nums[start]);
-
         }
-
     }
-
     vector<vector<int>> permute(vector<int>& nums) {
-
         result.push_back(nums);
-
-        recursive_permute(nums,1);
-
+        recursive_permute(nums, 1);
         return result;
-
     }
-
 };
